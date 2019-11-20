63f27b3ff2ab04f475f6f9565cf548b5c6987fe7
gmlangel <312260579@qq.com>
2019-11-20 11:39:45 +0800

    新增tls验证

--- RemoteDecition/service/appDelegate.go
+++ RemoteDecition/service/appDelegate.go
@@ -1,31 +1,38 @@
 package main
-import(
+
+import (
 	"fmt"
-	"github.com/kataras/iris"
-	"github.com/iris-contrib/middleware/cors"
+
 	"./proxy"
+	"github.com/iris-contrib/middleware/cors"
+	"github.com/kataras/iris"
 )
 
-
-func main(){
-	fmt.Println("RemoteDecition远程决策服务开始启动....");
+func main() {
+	debug := false //调试模式
+	fmt.Println("RemoteDecition远程决策服务开始启动....")
 	//初始化服务器
-	app := iris.New();
+	app := iris.New()
 	//修改跨域访问限制
 	crs := cors.New(cors.Options{
-        AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts."*"代表允许所有域访问，这是一个数组，可以添加多个域名
+		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts."*"代表允许所有域访问，这是一个数组，可以添加多个域名
 		AllowCredentials: true,
-		AllowedMethods:[]string{"HEAD","GET","POST"},
+		AllowedMethods:   []string{"HEAD", "GET", "POST"},
 	})
 	app.Use(crs)
 	//启动静态服务
-	staticPro := proxy.NewStaticManager(app);
-	staticPro.Start();
+	staticPro := proxy.NewStaticManager(app)
+	staticPro.Start()
 	//启动动态服务
 
 	//启动接口服务
-	webs := proxy.NewWebService(app);
-	webs.Start();
+	webs := proxy.NewWebService(app)
+	webs.Start()
 	//绑定服务器端口监听
-	app.Run(iris.Addr("0.0.0.0:8080"));
+	if debug == true {
+		app.Run(iris.Addr("0.0.0.0:8080"))
+	} else {
+		app.Run(iris.TLS("0.0.0.0:8080", "./1540854920368.pem", "./1540854920368.key"))
+	}
+
 }
